13cff339bcc26e8210c5aa36678530004a884b54
swarui <stevewarui88@gmail.com>
2024-10-28 15:12:39 +0300

    Coffee Landing Project

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,8 +1,8 @@
-import img from '../assets/Kafi.png'
+import img from '../assets/About.png'
 function AboutUs(){
     return(<>
 
-<div className="mt-10">
+<div className="mt-10 ">
         <h1
           style={{ fontFamily: "Poppins" }}
           className="text-center text-[1.5em] text-black "
@@ -11,13 +11,13 @@ function AboutUs(){
         </h1>
         <h2 style={{ fontFamily: "Poppins" }}
           className="text-center text-[0.8em] text-blue-600" >About Us</h2>
-    <div className="flex lg:flex-row flex-col">
+    <div className="flex lg:flex-row flex-col max-w-6xl mx-auto ">
         {/* Left Image body */}
         <div className="w-full lg:w-1/2">
-        <img src={img}  className='mt-[-20vh] lg:w-[550px] sm:w-[250px] md:w-[550px] ' alt="" />
+        <img src={img}  className='mt-[-20vh] ml-20 lg:w-[450px] sm:w-[250px] md:w-[550px] ' alt="" />
         </div>
         {/* Right text Area */}
-        <div className='w-full lg:w-1/2 mt-20  p-10'>
+        <div className='w-full lg:w-1/2 p-10'>
         <h2  style={{fontFamily:'Poppins'}} >Kahawa Ke, where coffee and comfort come together in perfect harmony. Nestled in the heart of Nairobi,Kenya.We pride ourselves on offering a unique experience for coffee lovers and travelers alike. At Kahawa Ke, we believe that great coffee isn't just a drink—it's a lifestyle.</h2>
         </div>
         </div>
