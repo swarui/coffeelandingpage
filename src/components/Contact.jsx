910056b16c56ba4bcc1644890c7f991a3a44ece7
swarui <stevewarui88@gmail.com>
2024-11-02 15:36:51 +0300

    Coffee landing page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,41 +3,63 @@ import Mocha from "../assets/Mocha.jpeg";
 function Contact() {
   return (
     <>
-         <div className="text-center mt-20 ">
-          {" "}
-          <h1
-            style={{ fontFamily: "Poppins" }}
-            className="text-center text-3xl font-bold text-[#3e2723] mb-2"
-          >
-            Contact us
-          </h1>
-          <h2
-            style={{ fontFamily: "Poppins" }}
-            className="text-center  text-lg text-blue-600  mb-6"
-          >
-            Reach Out
-          </h2>{" "}
-        </div>
-      <section className="flex flex-col md:flex-row items-center justify-center">
-   
+      <div className="text-center mt-20 ">
+        {" "}
+        <h1
+          style={{ fontFamily: "Poppins" }}
+          className="text-center text-3xl font-bold text-[#3e2723] mb-2"
+        >
+          Contact us
+        </h1>
+        <h2
+          style={{ fontFamily: "Poppins" }}
+          className="text-center  text-lg text-blue-600  mb-6"
+        >
+          Reach Out
+        </h2>{" "}
+      </div>
+      <section
+        style={{ fontFamily: "Poppins" }}
+        className="flex flex-col md:flex-row items-center justify-center p-8"
+      >
         {/* Contact From  */}
         <div className="w-full  md:w-1/2">
-          <form className="flex flex-col space-y-4">
-            <input
-              type="text"
-              placeholder="Your Name"
-              className="p-3 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-            />
+          <form className="flex flex-col space-y-4 p-10 bg-red-600 border rounded-lg ">
+            <div className="flex flex-row w-full space-x-4">
+              <div className="flex flex-col w-1/2">
+                <h2 className="">First Name</h2>
+
+                <input
+                  type="text"
+                  placeholder="First Name"
+                  className="   p-3 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                />
+              </div>
+              <div className="flex flex-col w-1/2">
+                <h2>Last Name</h2>
+                <input
+                  type="text"
+                  placeholder="Last Name"
+                  className=" flex flex-row p-3   rounded-md  "
+                />
+              </div>
+            </div>
+            <div className="flex flex-col ">
+            <h2>Email Adress</h2>
             <input
               type="email"
               placeholder="Your Email Address"
               className="p-3 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
             />
+            </div>
+            <div className="flex flex-col ">
+            <h2>Phone Number</h2>
             <input
               type="tel"
               placeholder="Your Phone Number"
               className="p-3 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
             />
+            </div>
             <textarea
               placeholder="Your Message"
               rows={4}
@@ -48,7 +70,7 @@ function Contact() {
         </div>
         {/* Image Section  */}
         <div className="w-full md:w-1/2 flex items-center justify-center">
-          <img src={Mocha} className="w-50 h-50" alt="" />
+          <img src={Mocha} className="w-3/4 " alt="" />
         </div>
       </section>
     </>
