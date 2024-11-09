3dc00f13c2525bd88ec991120665875126f76e07
swarui <stevewarui88@gmail.com>
2024-11-09 21:58:26 +0300

    Coffee landing page

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -4,7 +4,7 @@ import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-
+import  '../index.css'
 import Iced from '../assets/Iced.jpeg';
 import Latte from '../assets/Latte.jpeg';
 import Mocha from '../assets/Mocha.jpeg';
